f713be21f40d7dbed1134bf988a7adb1af086e32
CQUAN <cuuquan2703@gmail.com>
2023-12-04 20:00:38 +0700

    refactor: re-organize code

--- flaskr/routes/auth_route.py
+++ flaskr/routes/auth_route.py
@@ -22,7 +22,6 @@ def login():
      if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        # exist_user = UserModel.query.filter_by(username=username).all()
         exist_user = auth_service.find_user(username)
         if (len(exist_user) == 0):
             return {"success": False, "message":"Incorrect username"},401
@@ -50,29 +49,12 @@ def register():
         username = request.form['username']
         password = request.form['password']
         img= request.files['img']
-        print(request.files)
-        # data = img.read()
-        # mime = img.content_type
-        # print(data)
-        # print(mime)
-        file_name = secure_filename(f"{username}.jpeg")
-        upload_folder = os.path.join(os.getcwd(),STATIC,"uploads")
-        if not os.path.exists(upload_folder):
-            os.makedirs(upload_folder)
-        img.save(os.path.join(upload_folder,file_name))
-        # exist_user = UserModel.query.filter_by(username=username).all()
         exist_user = auth_service.find_user(username)
         if (len(exist_user) > 0):
             return {"success": False, "message":"User exist"},401
         else:       
             try:   
-            # hash_password = generate_password_hash(password,method=HASHING_METHOD).split("$")
-            # algo = hash_password[0]
-            # _salt = hash_password[1]
-            # new_password = hash_password[2]
-                (new_password,_salt,algo) = auth_service.create_hashing_password(password)['part']
-                print(f"{algo},{_salt},{new_password}")
-                new_user = UserModel(username=username, salt=_salt,password=new_password)
+                new_user = auth_service.register(username,password,img)
                 db.session.add(new_user)
                 db.session.commit()
                 return {"success": True, "message":"Successfully Register"}
@@ -84,7 +66,7 @@ def register():
 def login_by_face():
      if request.method == 'POST':
         username = request.form['username']
-        exist_user = UserModel.query.filter_by(username=username).all()
+        exist_user = auth_service.find_user(username)
         if (len(exist_user) == 0):
             return {"success": False, "message":"Incorrect username"},401
         else:
@@ -92,34 +74,10 @@ def login_by_face():
                 user = exist_user[0]
                 _username = user.username
                 img= request.files['img']
-                file_name = secure_filename(f"{username}_login.jpeg")
-                upload_folder = os.path.join(os.getcwd(),STATIC,"uploads")
-                if not os.path.exists(upload_folder):
-                    os.makedirs(upload_folder)
-                img.save(os.path.join(upload_folder,file_name))
 
-                login_img = cv2.imread(os.path.join(upload_folder,file_name))
-                gray_img = cv2.cvtColor(login_img, cv2.COLOR_BGR2GRAY)
-                print(os.path.join(os.getcwd(),"haarcascade_frontalface_default.xml"))
-                face_cascade = cv2.CascadeClassifier(os.path.join(os.getcwd(),"haarcascade_frontalface_default.xml"))
-                faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
-                if len(faces)==0:
-                    return {"success":False,"message":"No face in camera"}, 401
-                
-                login_img = face_recognition.load_image_file(os.path.join(upload_folder,file_name))
-                login_img_encoding = face_recognition.face_encodings(login_img)
-
-                _user_img = os.path.join(upload_folder,f"{username}.jpeg")
-                user_img = face_recognition.load_image_file(_user_img)
-
-                user_img_encoding = face_recognition.face_encodings(user_img)
-
-                if len(login_img_encoding)>0 and len(user_img_encoding)>0:
-                    matches = face_recognition.compare_faces(user_img_encoding,login_img_encoding[0])
-                    if any(matches):
-                        print(matches)
-                        return {"success":True,"message":"Success"}, 200
+                matches = auth_service.login_by_face(username,img)
+                if any(matches):
+                    return {"success":True,"message":"Success"}, 200
                 
                 return {"success":False,"message":"False"},401
             except:
--- flaskr/services/auth_service.py
+++ flaskr/services/auth_service.py
@@ -2,7 +2,10 @@ from ..models.user_model import UserModel
 import hashlib
 from ..config import STATIC, HASHING_METHOD
 from werkzeug.security import generate_password_hash,check_password_hash
-
+from werkzeug.utils import secure_filename
+import os
+import cv2
+import face_recognition
 class AuthService():
     def __init__(self,auth_model) -> None:
         self.auth_model = auth_model
@@ -40,3 +43,45 @@ class AuthService():
             'full_hash_password': hash_password,
             'part': (new_password,salt,algo)
         }
+    
+    def register(self,username,password,img):
+        file_name = secure_filename(f"{username}.jpeg")
+        upload_folder = os.path.join(os.getcwd(),STATIC,"uploads")
+        if not os.path.exists(upload_folder):
+            os.makedirs(upload_folder)
+        img.save(os.path.join(upload_folder,file_name))
+
+        (new_password,_salt,algo) = self.create_hashing_password(password)['part']
+        new_user = self.auth_model(username=username, salt=_salt,password=new_password)
+        return new_user
+    
+    def find_user(self,username):
+        return self.auth_model.query.filter_by(username=username).all()
+
+    def login_by_face(self,username,img):
+        file_name = secure_filename(f"{username}_login.jpeg")
+        upload_folder = os.path.join(os.getcwd(),STATIC,"uploads")
+        if not os.path.exists(upload_folder):
+            os.makedirs(upload_folder)
+        img.save(os.path.join(upload_folder,file_name))
+
+        login_img = cv2.imread(os.path.join(upload_folder,file_name))
+        gray_img = cv2.cvtColor(login_img, cv2.COLOR_BGR2GRAY)
+        print(os.path.join(os.getcwd(),"haarcascade_frontalface_default.xml"))
+        face_cascade = cv2.CascadeClassifier(os.path.join(os.getcwd(),"haarcascade_frontalface_default.xml"))
+        faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+        if len(faces)==0:
+            return {"success":False,"message":"No face in camera"}, 401
+        
+        login_img = face_recognition.load_image_file(os.path.join(upload_folder,file_name))
+        login_img_encoding = face_recognition.face_encodings(login_img)
+
+        _user_img = os.path.join(upload_folder,f"{username}.jpeg")
+        user_img = face_recognition.load_image_file(_user_img)
+
+        user_img_encoding = face_recognition.face_encodings(user_img)
+
+        if len(login_img_encoding)>0 and len(user_img_encoding)>0:
+            matches = face_recognition.compare_faces(user_img_encoding,login_img_encoding[0])
+            return matches
